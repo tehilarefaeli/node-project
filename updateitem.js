d6ed5c31a1f8e3a2a1e17f9962704181c2c3d65b
SariLev <sarile200@gmail.com>
2023-12-02 23:04:48 +0200

    j

--- updateitem.js
+++ updateitem.js
@@ -1,7 +1,7 @@
 function setValues() {
     let itemFromStorage = window.localStorage.getItem('item');
     let itemData = JSON.parse(itemFromStorage);
-    document.getElementById('item_name').value = itemData.name;
+    document.getElementById('name').value = itemData.name;
     document.getElementById('item_cost').value = itemData.cost;
     document.getElementById('item_img').value = itemData.img;
     document.getElementById('item_category').value = itemData.category;
